6d87b962c10f17539a8c4ce8bd1ce19d302c4982
Devin Smith <devinsmith@deephaven.io>
2022-01-19 07:38:18 -0800

    Make note about pip / setuptools being upgraded standalone

--- server/04-python-dev-base/install.sh
+++ server/04-python-dev-base/install.sh
@@ -18,7 +18,8 @@ dpkg --list
 
 python3 -m venv /opt/build-venv
 
-/opt/build-venv/bin/python -m pip install -q --no-cache-dir --upgrade pip
-/opt/build-venv/bin/python -m pip install -q --no-cache-dir --upgrade setuptools
+# Upgrade pip and setuptools standalone.
+# Can replace with venv module option `--upgrade-deps` if newer python3-venv becomes available.
+/opt/build-venv/bin/python -m pip install -q --no-cache-dir --upgrade pip setuptools
 /opt/build-venv/bin/python -m pip install -q --no-cache-dir --upgrade wheel
 /opt/build-venv/bin/python -m pip list
--- server/04-server-base/install.sh
+++ server/04-server-base/install.sh
@@ -9,7 +9,8 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 python3 -m venv /opt/deephaven-venv
 
-/opt/deephaven-venv/bin/python -m pip install -q --no-cache-dir --upgrade pip
-/opt/deephaven-venv/bin/python -m pip install -q --no-cache-dir --upgrade setuptools
+# Upgrade pip and setuptools standalone.
+# Can replace with venv module option `--upgrade-deps` if newer python3-venv becomes available.
+/opt/deephaven-venv/bin/python -m pip install -q --no-cache-dir --upgrade pip setuptools
 /opt/deephaven-venv/bin/python -m pip install -q --no-cache-dir -r "${__dir}/requirements.txt"
 /opt/deephaven-venv/bin/python -m pip list
