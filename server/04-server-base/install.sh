eaf4c1957afdc286ef89f95b664f34d5bbba4651
Devin Smith <devinsmith@deephaven.io>
2022-09-19 16:16:25 -0700

    try only binary

--- server/04-server-base/install.sh
+++ server/04-server-base/install.sh
@@ -14,5 +14,5 @@ source /opt/deephaven-venv/bin/activate
 # Can replace with venv module option `--upgrade-deps` if newer python3-venv becomes available.
 python -m pip install -q --no-cache-dir --upgrade pip setuptools
 python -m pip install -q --no-cache-dir --upgrade wheel
-python -m pip install -q --no-cache-dir -r "${__dir}/requirements.txt"
+python -m pip install -q --no-cache-dir --only-binary=:all: -r "${__dir}/requirements.txt"
 python -m pip list
