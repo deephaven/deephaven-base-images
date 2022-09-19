3d6856a1855a94d891620563633589c55becd526
Devin Smith <devinsmith@deephaven.io>
2022-09-19 16:01:07 -0700

    Install wheel package

--- server/04-server-base/install.sh
+++ server/04-server-base/install.sh
@@ -13,5 +13,6 @@ source /opt/deephaven-venv/bin/activate
 # Upgrade pip and setuptools standalone.
 # Can replace with venv module option `--upgrade-deps` if newer python3-venv becomes available.
 python -m pip install -q --no-cache-dir --upgrade pip setuptools
+python -m pip install -q --no-cache-dir --upgrade wheel
 python -m pip install -q --no-cache-dir -r "${__dir}/requirements.txt"
 python -m pip list
