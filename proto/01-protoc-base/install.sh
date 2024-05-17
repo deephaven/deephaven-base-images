06ec041b4c05eb812a3ad552cdbf8b0df6852870
Cristian Ferretti <jcferretti2020@gmail.com>
2024-05-17 13:01:32 -0400

    Update protoc java plugin; refactor repetition of versions in install.sh

--- proto/01-protoc-base/install.sh
+++ proto/01-protoc-base/install.sh
@@ -23,23 +23,29 @@ python3 -m pip install -q --no-cache-dir --upgrade setuptools
 python3 -m pip install -q --no-cache-dir -r "${__dir}/requirements.txt"
 python3 -m pip list
 
-wget -q "https://github.com/protocolbuffers/protobuf/releases/download/v26.1/protoc-26.1-linux-x86_64.zip"
-wget -q "https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/1.50.2/protoc-gen-grpc-java-1.50.2-linux-x86_64.exe"
-wget -q "https://github.com/protocolbuffers/protobuf-javascript/releases/download/v3.21.2/protobuf-javascript-3.21.2-linux-x86_64.zip"
+pbuf_ver=26.1
+wget -q "https://github.com/protocolbuffers/protobuf/releases/download/v$(pbuf_ver)/protoc-$(pbuf_ver)-linux-x86_64.zip"
+
+java_plugin_ver=1.63.1
+wget -q "https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/$(java_plugin_ver)/protoc-gen-grpc-java-$(java_plugin_ver)-linux-x86_64.exe"
+
+js_ver=3.21.2
+wget -q "https://github.com/protocolbuffers/protobuf-javascript/releases/download/v$(js_ver)/protobuf-javascript-$(js_ver)-linux-x86_64.zip"
 sha256sum --check "${__dir}/checksums.txt"
 
-unzip protoc-26.1-linux-x86_64.zip -d /opt/protoc
-rm protoc-26.1-linux-x86_64.zip
+pbuf_zip=protoc-$(pbuf_ver)-linux-x86_64.zip
+unzip "$pbuf_zip" -d /opt/protoc
+rm "$pbuf_zip"
 
 mkdir -p /opt/java/bin
 
-mv protoc-gen-grpc-java-1.50.2-linux-x86_64.exe /opt/java/bin/protoc-gen-grpc-java
+mv "protoc-gen-grpc-java-$(java_plugin_ver)-linux-x86_64.exe" /opt/java/bin/protoc-gen-grpc-java
 chmod +x /opt/java/bin/protoc-gen-grpc-java
 
-unzip protobuf-javascript-3.21.2-linux-x86_64.zip -d jsplugin_temp
+unzip "protobuf-javascript-$(js_ver)-linux-x86_64.zip" -d jsplugin_temp
 # move somewhere that is guaranteed to be on the PATH so protoc can find it
 mv jsplugin_temp/bin/protoc-gen-js /usr/local/bin/
-rm -r protobuf-javascript-3.21.2-linux-x86_64.zip jsplugin_temp
+rm -r "protobuf-javascript-$(js_ver)-linux-x86_64.zip" jsplugin_temp
 
 mkdir -p /usr/src/app
 cp "${__dir}/package.json" /usr/src/app
