b5db8c19d95650e497da1dec5dd9474de90f89a5
Cristian Ferretti <jcferretti2020@gmail.com>
2024-05-17 14:07:49 -0400

    Fix issues with $() versus ${}

--- proto/01-protoc-base/install.sh
+++ proto/01-protoc-base/install.sh
@@ -24,28 +24,28 @@ python3 -m pip install -q --no-cache-dir -r "${__dir}/requirements.txt"
 python3 -m pip list
 
 pbuf_ver=26.1
-wget -q "https://github.com/protocolbuffers/protobuf/releases/download/v$(pbuf_ver)/protoc-$(pbuf_ver)-linux-x86_64.zip"
+wget -q "https://github.com/protocolbuffers/protobuf/releases/download/v${pbuf_ver}/protoc-${pbuf_ver}-linux-x86_64.zip"
 
 java_plugin_ver=1.63.1
-wget -q "https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/$(java_plugin_ver)/protoc-gen-grpc-java-$(java_plugin_ver)-linux-x86_64.exe"
+wget -q "https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/${java_plugin_ver}/protoc-gen-grpc-java-${java_plugin_ver}-linux-x86_64.exe"
 
 js_ver=3.21.2
-wget -q "https://github.com/protocolbuffers/protobuf-javascript/releases/download/v$(js_ver)/protobuf-javascript-$(js_ver)-linux-x86_64.zip"
+wget -q "https://github.com/protocolbuffers/protobuf-javascript/releases/download/v${js_ver}/protobuf-javascript-${js_ver}-linux-x86_64.zip"
 sha256sum --check "${__dir}/checksums.txt"
 
-pbuf_zip=protoc-$(pbuf_ver)-linux-x86_64.zip
+pbuf_zip=protoc-${pbuf_ver}-linux-x86_64.zip
 unzip "$pbuf_zip" -d /opt/protoc
 rm "$pbuf_zip"
 
 mkdir -p /opt/java/bin
 
-mv "protoc-gen-grpc-java-$(java_plugin_ver)-linux-x86_64.exe" /opt/java/bin/protoc-gen-grpc-java
+mv "protoc-gen-grpc-java-${java_plugin_ver}-linux-x86_64.exe" /opt/java/bin/protoc-gen-grpc-java
 chmod +x /opt/java/bin/protoc-gen-grpc-java
 
-unzip "protobuf-javascript-$(js_ver)-linux-x86_64.zip" -d jsplugin_temp
+unzip "protobuf-javascript-${js_ver}-linux-x86_64.zip" -d jsplugin_temp
 # move somewhere that is guaranteed to be on the PATH so protoc can find it
 mv jsplugin_temp/bin/protoc-gen-js /usr/local/bin/
-rm -r "protobuf-javascript-$(js_ver)-linux-x86_64.zip" jsplugin_temp
+rm -r "protobuf-javascript-${js_ver}-linux-x86_64.zip" jsplugin_temp
 
 mkdir -p /usr/src/app
 cp "${__dir}/package.json" /usr/src/app
